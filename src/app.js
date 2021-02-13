85532d6cd33d620265e6b1b2d3115e8e0b55a31b
cpramshad <cpramshad@gmail.com>
2021-02-13 15:36:26 +0400

    Setup add for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const postmanRequest = require('postman-request');
 // const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -107,6 +108,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
